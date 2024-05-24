6f58514349e4dbe2645d53c2460f0ad86cb2baa3
Bhav Beri <bhavberi@gmail.com>
2024-05-24 14:36:21 +0530

    Remove unwanted console log

--- web/src/routes/student-bodies/+page.ts
+++ web/src/routes/student-bodies/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = async ({ parent, data }) => {
   await parent()
   let { page_server_data } = data;
-  console.log(JSON.stringify(data,null,2));
+  // console.log(JSON.stringify(data,null,2));
   return {
     page_server_data
   }
