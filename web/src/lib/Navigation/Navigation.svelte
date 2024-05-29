4e785035cd79925d9a9d315959ff9b5157ad233a
Bhav Beri <bhavberi@gmail.com>
2024-05-29 11:55:40 +0530

    Change color of hover in menu in mobile view

--- web/src/lib/Navigation/Navigation.svelte
+++ web/src/lib/Navigation/Navigation.svelte
@@ -1,30 +1,45 @@
-<script>
+<script lang="ts">
 	import { getDrawerStore, LightSwitch } from '@skeletonlabs/skeleton';
 	import { page } from '$app/stores';
 	let current_page = $page.url.pathname;
+	import '../../app.postcss';
 
 	const drawerStore = getDrawerStore();
 
 	function drawerClose() {
 		drawerStore.close();
 	}
+
+	import { modeCurrent } from '@skeletonlabs/skeleton';
+
+	let color: string;
+
+	$: {
+		if ($modeCurrent == false) {
+			color = 'white';
+		} else {
+			color = 'black';
+		}
+	}
 </script>
 
-<nav class="list px-2 py-4">
-	<ul>
-		<li class={current_page === '/' ? 'active' : ''}>
+<nav class="list px-2 py-4" style={`color: ${color}`}>
+	<ul class="menu">
+		<li class={current_page === '/' ? 'variant-filled-primary' : ''}>
 			<a href="/" on:click={drawerClose}>Home</a>
 		</li>
-		<li class={current_page === '/supervisory-bodies' ? 'active' : ''}>
+		<li class={current_page === '/supervisory-bodies' ? 'variant-filled-primary' : ''}>
 			<a href="/supervisory-bodies" on:click={drawerClose}>Supervisory Bodies</a>
 		</li>
-		<li class={current_page === '/student-bodies' ? 'active' : ''}>
+		<li class={current_page === '/student-bodies' ? 'variant-filled-primary' : ''}>
 			<a href="/student-bodies" on:click={drawerClose}>Student Bodies</a>
 		</li>
-		<li class={current_page === '/calendar' ? 'active' : ''}>
+		<li class={current_page === '/calendar'
+		? 'variant-filled-primary'
+		: ''}>
 			<a href="/calendar" on:click={drawerClose}>Calendar</a>
 		</li>
-		<li class={current_page === '/gallery' ? 'active' : ''}>
+		<li class={current_page === '/gallery' ? 'variant-filled-primary' : ''}>
 			<a href="/gallery" on:click={drawerClose}>Gallery</a>
 		</li>
 	</ul>
@@ -36,10 +51,6 @@
 </nav>
 
 <style lang="postcss">
-	.active {
-		background-color: #bda5d5;
-	}
-
 	.list li {
 		margin: 5px;
 		padding: 5px;
@@ -55,8 +66,12 @@
 		color: inherit;
 	}
 
-	.list li:not(:last-child):hover {
-		background-color: #e3d3f3;
+	.list .menu li:hover {
+		background-color: #d4b4f5;
+	}
+
+	.dark .list .menu li:hover {
+		background-color: #685703;
 	}
 
 	.switch {
