999c938df50b4a49305261260bdab092dab46fa2
Bhav Beri <43399374+bhavberi@users.noreply.github.com>
2024-09-24 21:56:59 +0530

    Remove deprecated recent events query

--- web/src/lib/gql/queries/events.ts
+++ web/src/lib/gql/queries/events.ts
@@ -1,18 +1,5 @@
 import gql from 'graphql-tag';
 
-export const GET_RECENT_EVENTS = gql`
-	query RecentEvents {
-		recentEvents {
-			_id
-			name
-			code
-			clubid
-			datetimeperiod
-			poster
-		}
-	}
-`;
-
 export const GET_ALL_EVENTS = gql`
 	query Events($clubid: String, $public: Boolean, $limit: Int) {
 		events(clubid: $clubid, public: $public, limit: $limit) {
